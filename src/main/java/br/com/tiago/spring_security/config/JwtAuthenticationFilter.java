b5d0dfab164188c44ce338704fe0f17feb02bd65
Tiago Malanczen <malanczen13@gmail.com>
2026-09-04 20:59:46 -0300

    feat : adiciona classe SecurityConfiguration

--- src/main/java/br/com/tiago/spring_security/config/JwtAuthenticationFilter.java
+++ src/main/java/br/com/tiago/spring_security/config/JwtAuthenticationFilter.java
@@ -41,8 +41,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
     }
     }
-
-
     filterChain.doFilter(request, response);
     }
 }
